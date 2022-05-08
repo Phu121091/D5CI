2bd4b396625f4cb977ceb2ae55c87faadadcf03f
Phu121091 <nguyentrongphuu@gmail.com>
2022-05-08 22:45:17 +0700

    first commit

---
+++ src/homework/components/Todo.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Todo = ({ category, name, price,description,rate, id,image }) => {
+  return (
+    <div className="todo">
+      <div className="info">
+      <p className="id" style={{ color: "red" }}>
+        {id}
+      </p>
+      <p className="name" style={{ color: "orange" }}>
+        Name : {name}
+      </p>
+      <p className="price" style={{ color: "green" }}>
+        Price : {price}
+      </p>
+      <p>Description: {description}</p>
+      <p>Category : {category}</p>
+      <p>Rate : {rate.rate}- Count : {rate.count}</p>
+      </div>
+      <img src={image}></img>
+      {/* <button onClick={() => handleDelete(id)}>DELETE</button> */}
+    </div>
+  );
+};
+
+export default Todo;
